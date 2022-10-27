d8c09336b90d6a452a3930f4096c302b59fe4a2f
Cvam1999 <108715715+Cvam1999@users.noreply.github.com>
2022-10-27 09:46:17 +0530

    Delete Spring-MCV-Example_21 directory

--- Spring-MCV-Example_21/Spring-MCV-Example/src/main/java/com/cg/spring/mvc/training/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.spring.mvc.training.service;
-
-import com.cg.spring.mvc.training.models.Register;
-
-public interface RegisterService {
-	
-	Register registerUer(Register register);
-
-}
--- Spring-MCV-Example_21/Spring-MCV-Example/src/main/webapp/WEB-INF/jsp/banner.jsp
+++
@@ -1,4 +0,0 @@
-	<div style="position:absolute; top:0px; height:100px; background-color:pink; width:100%;" align="center">
-			<h1>Employee Management</h1>
-	</div>
-	
